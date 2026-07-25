b7132cae2aa40ed90295989f607aa9fd495680d7
Mahdi Baghbani <mahdi-baghbani@azadehafzar.io>
2026-07-25 23:47:06 +0000

    test(spec): sync ocm wire literal line allowlist
    Signed-off-by: Mahdi Baghbani <mahdi-baghbani@azadehafzar.io>

--- internal/components/ocm/spec/wire_constants_test.go
+++ internal/components/ocm/spec/wire_constants_test.go
@@ -74,7 +74,7 @@ var wireLiteralAllowlist = map[string]map[int]struct{}{
 		58: {},
 	},
 	"internal/platform/config/loader.go": {
-		662: {},
+		668: {},
 	},
 	"internal/platform/crypto/sigparams/label.go": {
 		4: {},
